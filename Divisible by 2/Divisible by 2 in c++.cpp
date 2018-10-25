16c4041da2b59830134f17062b20d901fd0b3b43
shambhav11 <39239330+shambhav11@users.noreply.github.com>
2018-10-25 19:28:51 +0530

    Create Divisible by 2 in c++.cpp

---
+++ Divisible by 2/Divisible by 2 in c++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int i;
+      cout<<"Enter Number to be checked \n";
+      cin>>i;
+      if(i%2==0)
+        cout<<"Divisible by 2";
+      else
+        coud<<"Not Divisible by 2";
+      
+      return 0;
+      
+}
